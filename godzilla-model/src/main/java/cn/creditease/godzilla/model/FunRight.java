64af2693bbb3f27b3c9c754ec8f71b6d90cdf403
ZhongweiLee <576594216@qq.com>
2015-06-10 19:42:14 +0800

    生成model and dao

---
+++ godzilla-model/src/main/java/cn/creditease/godzilla/model/FunRight.java
@@ -0,0 +1,95 @@
+package cn.creditease.godzilla.model;
+
+import java.util.Date;
+
+public class FunRight {
+    private Long id;
+
+    private String userName;
+
+    private String projectCode;
+    
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String createBy;
+
+    private String realName;
+
+    private String projectName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getProjectCode() {
+        return projectCode;
+    }
+
+    public void setProjectCode(String projectCode) {
+        this.projectCode = projectCode;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+}
